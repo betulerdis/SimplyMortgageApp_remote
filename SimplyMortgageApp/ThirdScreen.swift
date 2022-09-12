7a1f4d08fb557af043b8129061d76345e139d854
Betul Erdis <betul.krl@gmail.com>
2022-09-11 21:57:21 -0400

    call button added

--- SimplyMortgageApp/ThirdScreen.swift
+++ SimplyMortgageApp/ThirdScreen.swift
@@ -11,9 +11,20 @@ class ThirdScreen: UIViewController {
     
     @IBOutlet weak var newCalculation: UIButton!
     
+    @IBOutlet weak var callButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    @IBAction func simplyCall(_ sender: Any) {
+        
+        if let url = URL(string: "tel://3659961634"){
+            if UIApplication.shared.canOpenURL(url){
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }else{
+                
+            }
+        }
     }
 }
